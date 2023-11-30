c67e6197fab8a51c8773cba6530b2f31ab4e9c80
Attuluru <143151335+Attuluru@users.noreply.github.com>
2023-11-29 23:56:48 -0600

    done

--- routes/bag.js
+++ routes/bag.js
@@ -17,11 +17,11 @@ module.exports = router;
 /* GET detail bag page */
 router.get('/detail', bag_controlers.bag_view_one_Page);
 /* GET create bag page */
-router.get('/create', bag_controlers.bag_create_Page);
+router.get('/create', secured, bag_controlers.bag_create_Page);
 /* GET create update page */
-router.get('/update', bag_controlers.bag_update_Page);
+router.get('/update', hsecured, bag_controlers.bag_update_Page);
 /* GET delete bag page */
-router.get('/delete', bag_controlers.bag_delete_Page);
+router.get('/delete', secured ,bag_controlers.bag_delete_Page);
 
 
 
